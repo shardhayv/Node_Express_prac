aa0ed32817f57d4d86f2930f6a9dc3de29d79d5f
shardhayv <shardhayvatshyayan7@gmail.com>
2023-07-02 23:44:46 +0545

    OS MODULE EXPLORED

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const os = require("os");
+
+// info about current user
+const userInfo = os.userInfo();
+
+console.log(userInfo);
+
+// checking system uptime
+
+console.log(`System uptime is ${os.uptime()} seconds`);
+
+const osInfo = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMemory: os.freemem(),
+};
+
+console.log(osInfo);
